dd312146e0b0d3a64be3bc8c93d5f7a28075e914
Ekaterina Shevchenko <eshevchenko8@gmail.com>
2016-11-12 13:45:19 +0400

    2c regexp refactoring

--- 2c/func.js
+++ 2c/func.js
@@ -1,6 +1,6 @@
 export default function getUsername(input) {
   const regProtocol = /^\w*:?\/\//i;
-  const regDomain = /^(?:[а-я\w\d\-\.]+)?\//i;
+  const regDomain = /^([а-я\w\d\-\.]+)?\//i;
   const regUsername = /([\w\d_\.]+)/i;
 
   const result = input.trim().replace(regProtocol, '').replace(regDomain, '').match(regUsername);
